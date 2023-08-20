67cb5fbcb3ce69ea4053c2f58b1ecb98b87a5bc3
Prasad750 <prasadchaudhari750@gmail.com>
2023-08-20 23:50:22 +0530

    Attempt3

--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -32,26 +32,29 @@ public class SavingsAccount extends BankAccount{
         // Might throw the following errors:
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
-        if(amount>maxWithdrawalLimit)
+        if(amount<=maxWithdrawalLimit)
+        {
+             super.withdraw(amount);
+        }
+        else
         {
             throw new Exception( "Maximum Withdraw Limit Exceed");
         }
-        super.withdraw(amount);
 
     }
 
     public double getSimpleInterest(int years){
         // Return the final amount considering that bank gives simple interest on current amount
-        return this.getBalance()*(1+this.rate*years)/100;
+        return this.getBalance()*(1+(this.rate*years)/100);
     }
 
     public double getCompoundInterest(int times, int years){
         // Return the final amount considering that bank gives compound interest on current amount given times per year
         double p=this.getBalance();
-        double RatePerTimes=rate/(times*1.0);
-        double TimesYear=times*years;
+        double RatePerTimes=rate/(times*100);
+        double timesYear=times*years;
 
-        return p * Math.pow(1+RatePerTimes,TimesYear);
+        return p * Math.pow(1+RatePerTimes,timesYear);
 
     }
 
